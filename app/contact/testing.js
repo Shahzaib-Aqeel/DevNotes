7ca24b8e79c0203a769838839f3f880e37efd814
Shahzaib-Aqeel <shabbych366@gmail.com>
2025-10-24 03:59:43 -0700

    Initial Commit

--- app/contact/testing.js
+++
@@ -1,4 +0,0 @@
-console.log(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID);
-console.log(process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID);
-console.log(process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY);
-console.log(process.env.MONGODB_URI);
